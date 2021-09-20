42a97a313df88806163df6d7b192236ba00832dd
Harry Parkes <harrydparkes@protonmail.com>
2021-09-20 21:29:06 +1200

    dope new MoGoLift class

---
+++ include/MoGoLift.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "main.h"
+#include "StepperPID.h"
+
+class MoGoLift
+{
+public:
+    MoGoLift(uint8_t leftPort, uint8_t rightPort, StepperPID& _pid, ControllerButton *const _upButton, ControllerButton *const _downButton);
+
+    void RunUserControl();
+
+private:
+    struct side
+    {
+        side(int _portNumber);
+
+        Motor motor;
+        const IntegratedEncoder encoder;
+    };
+    
+    side left;
+    side right;
+
+    StepperPID pid;
+
+    ControllerButton *const upButton;
+    ControllerButton *const downButton;
+};
---
+++ src/MoGoLift.cpp
@@ -0,0 +1,40 @@
+#include "MoGoLift.h"
+
+MoGoLift::MoGoLift(uint8_t _leftPort, uint8_t _rightPort, StepperPID& _pid, ControllerButton *const _upButton, ControllerButton *const _downButton)
+    : left(_leftPort), right(_rightPort), pid(_pid), upButton(_upButton), downButton(_downButton)
+{
+    left.motor.setGearing(AbstractMotor::gearset::red);
+    right.motor.setGearing(AbstractMotor::gearset::red);
+    left.motor.setReversed(true);
+}
+
+#define PID_INCREMENT 2
+
+void MoGoLift::RunUserControl()
+{
+    if(upButton->isPressed())
+        pid.IncrementTarget(PID_INCREMENT);
+
+    else if(downButton->isPressed())
+        pid.IncrementTarget(-PID_INCREMENT);
+
+    else if (upButton->changedToReleased())
+    {
+        int leftSensor = left.encoder.get();
+        int rightSensor = right.encoder.get();
+        pid.SetTarget( (leftSensor > rightSensor) ? leftSensor : rightSensor );
+    }
+    else if (downButton->changedToReleased())
+    {
+        int leftSensor = left.encoder.get();
+        int rightSensor = right.encoder.get();
+        pid.SetTarget( (leftSensor < rightSensor) ? leftSensor : rightSensor );
+    }
+
+
+    cout << endl << "Left: " << endl;
+    left.motor.moveVoltage( pid.Calculate( left.encoder.get() ) );
+
+    cout << endl << "Right: " << endl;
+    right.motor.moveVoltage( pid.Calculate( right.encoder.get() ) );
+}
