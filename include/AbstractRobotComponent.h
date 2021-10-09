d9b9a691a74fdf176a1402151d2f4a320e721c7a
Harry Parkes <harrydparkes@protonmail.com>
2021-10-09 14:08:54 +1300

    Abstract component class (Unfinished)

---
+++ include/AbstractRobotComponent.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "MotorContainer.h"
+#include "PID.h"
+
+class AbstractRobotComponent
+{
+    AbstractRobotComponent(int8_t _firstMotorPort, int8_t _secondMotorPort, PID _pid);
+
+    virtual void RunUserControl();
+
+    virtual void RunPID();
+
+    virtual void SetTarget(int _target);
+    virtual void ResetSensors();
+
+private:
+    MotorContainer first;
+    MotorContainer second;
+
+    PID pid;
+};
